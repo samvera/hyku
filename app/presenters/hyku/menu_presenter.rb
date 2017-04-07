a3e6220fc8452b95481cc205fc02dcc6a06dfbee
Joe Atzberger <ohiocore@gmail.com>
2017-04-06 17:23:35 -0700

    This resolves the problems with the sidebar "forgetting" expanded state
    Really was one misnamed `open` method and accounting for a renaming of a
    particular page underneath us.

--- app/presenters/hyku/menu_presenter.rb
+++ app/presenters/hyku/menu_presenter.rb
@@ -21,13 +21,13 @@ module Hyku
     end
 
     # Returns true if the current controller happens to be one of the controllers that deals
-    # with repository activity  This is used to keep the parent section on the sidebar open.
+    # with repository activity. This is used to keep the parent section on the sidebar open.
     def repository_activity_section?
-      %w(admin status).include?(controller_name)
+      %w(admin dashboard status).include?(controller_name)
     end
 
     # Returns true if the current controller happens to be one of the controllers that deals
-    # with user activity  This is used to keep the parent section on the sidebar open.
+    # with user activity. This is used to keep the parent section on the sidebar open.
     def user_activity_section?
       # we're using a case here because we need to differentiate UsersControllers
       # in different namespaces (Hyrax & Admin)

8dba816439e5071bd71bae7df47608e45266e899
Justin Coyne <jcoyne@justincoyne.com>
2017-05-16 09:26:26 -0700

    Document a monkeypatch

--- lib/active_job/queue_adapters/better_active_elastic_job_adapter.rb
+++ lib/active_job/queue_adapters/better_active_elastic_job_adapter.rb
@@ -4,6 +4,9 @@ module ActiveJob
   module QueueAdapters
     class BetterActiveElasticJobAdapter < ActiveElasticJobAdapter
       class << self
+        # We're replacing this method so that we invoke the SQS::Client with no parameters, effectively
+        # using the default credentials set by ElasticBeanstalk.
+        # Upstream method: https://github.com/tawan/active-elastic-job/blob/8a870e6ca4542438d2ed167ed2cb9b1473ee702d/lib/active_job/queue_adapters/active_elastic_job_adapter.rb#L168
         def aws_sqs_client
           @aws_sqs_client ||= Aws::SQS::Client.new
         end

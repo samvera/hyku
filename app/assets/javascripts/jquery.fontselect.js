cf757744b1d6ec617d1c7acd5fd7ffd9574e1a03
kellyChex <kelly@notch8.com>
2019-03-12 11:17:36 -0700

    Fix hound errors

--- app/assets/javascripts/jquery.fontselect.js
+++ app/assets/javascripts/jquery.fontselect.js
@@ -1,6 +1,6 @@
 $(document).on('turbolinks:load', function() {
-  $("#admin_appearance_body_font").fontselect({lookahead: 20})
-  $("#admin_appearance_headline_font").fontselect({lookahead: 20})
+  $("#admin_appearance_body_font").fontselect({lookahead: 20});
+  $("#admin_appearance_headline_font").fontselect({lookahead: 20});
 });
 
 /*
@@ -13,7 +13,7 @@ $(document).on('turbolinks:load', function() {
 
 (function($){
 
-  $.fn.fontselect = function(options) {  
+  $.fn.fontselect = function(options) {
 
      var __bind = function(fn, me){ return function(){ return fn.apply(me, arguments); }; };
 
@@ -112,13 +112,13 @@ $(document).on('turbolinks:load', function() {
       "La+Belle+Aurore",
       "Lato:100",
       "Lato:100italic",
-      "Lato:300", 
+      "Lato:300",
       "Lato",
-      "Lato:bold",  
+      "Lato:bold",
       "Lato:900",
       "League+Script",
-      "Lekton",  
-      "Limelight",  
+      "Lekton",
+      "Limelight",
       "Lobster",
       "Lobster Two",
       "Lora",
@@ -143,8 +143,8 @@ $(document).on('turbolinks:load', function() {
       "Monofett",
       "Molengo",
       "Mountains of Christmas",
-      "Muli:300", 
-      "Muli", 
+      "Muli:300",
+      "Muli",
       "Neucha",
       "Neuton",
       "News+Cycle",
@@ -176,7 +176,7 @@ $(document).on('turbolinks:load', function() {
       "Reenie+Beanie",
       "Pacifico",
       "Patrick+Hand",
-      "Paytone+One", 
+      "Paytone+One",
       "Permanent+Marker",
       "Philosopher",
       "Play",
@@ -242,9 +242,9 @@ $(document).on('turbolinks:load', function() {
       lookahead: 2,
       api: '//fonts.googleapis.com/css?family='
     };
-    
+
     var Fontselect = (function(){
-    
+
       function Fontselect(original, o){
         this.$original = $(original);
         this.options = o;
@@ -259,47 +259,47 @@ $(document).on('turbolinks:load', function() {
           this.addFontLink(font);
         }
       }
-      
+
       Fontselect.prototype.bindEvents = function(){
-      
+
         $('li', this.$results)
         .click(__bind(this.selectFont, this))
         .mouseenter(__bind(this.activateFont, this))
         .mouseleave(__bind(this.deactivateFont, this));
-        
+
         $('span', this.$select).click(__bind(this.toggleDrop, this));
         this.$arrow.click(__bind(this.toggleDrop, this));
       };
-      
+
       Fontselect.prototype.toggleDrop = function(ev){
-        
+
         if(this.active){
           this.$element.removeClass('font-select-active');
           this.$drop.hide();
           clearInterval(this.visibleInterval);
-          
+
         } else {
           this.$element.addClass('font-select-active');
           this.$drop.show();
           this.moveToSelected();
           this.visibleInterval = setInterval(__bind(this.getVisibleFonts, this), 500);
         }
-        
+
         this.active = !this.active;
       };
-      
+
       Fontselect.prototype.selectFont = function(){
-        
+
         var font = $('li.active', this.$results).data('value');
         this.$original.val(font).change();
         this.updateSelected();
         this.toggleDrop();
       };
-      
+
       Fontselect.prototype.moveToSelected = function(){
-        
+
         var $li, font = this.$original.val();
-        
+
         if (font){
           $li = $("li[data-value='"+ font +"']", this.$results);
         } else {
@@ -308,25 +308,25 @@ $(document).on('turbolinks:load', function() {
 
         this.$results.scrollTop($li.addClass('active').position().top);
       };
-      
+
       Fontselect.prototype.activateFont = function(ev){
         $('li.active', this.$results).removeClass('active');
         $(ev.currentTarget).addClass('active');
       };
-      
+
       Fontselect.prototype.deactivateFont = function(ev){
-        
+
         $(ev.currentTarget).removeClass('active');
       };
-      
+
       Fontselect.prototype.updateSelected = function(){
-        
+
         var font = this.$original.val();
         $('span', this.$element).text(this.toReadable(font)).css(this.toStyle(font));
       };
-      
+
       Fontselect.prototype.setupHtml = function(){
-      
+
         this.$original.empty().hide();
         this.$element = $('<div>', {'class': this.options.style});
         this.$arrow = $('<div><b></b></div>');
@@ -336,43 +336,43 @@ $(document).on('turbolinks:load', function() {
         this.$original.after(this.$element.append(this.$select.append(this.$arrow)).append(this.$drop));
         this.$drop.append(this.$results.append(this.fontsAsHtml())).hide();
       };
-      
+
       Fontselect.prototype.fontsAsHtml = function(){
-        
+
         var l = fonts.length;
         var r, s, h = '';
-        
+
         for(var i=0; i<l; i++){
           r = this.toReadable(fonts[i]);
           s = this.toStyle(fonts[i]);
           h += '<li data-value="'+ fonts[i] +'" style="font-family: '+s['font-family'] +'; font-weight: '+s['font-weight'] +'">'+ r +'</li>';
         }
-        
+
         return h;
       };
-      
+
       Fontselect.prototype.toReadable = function(font){
         return font.replace(/[\+|:]/g, ' ');
       };
-      
+
       Fontselect.prototype.toStyle = function(font){
         var t = font.split(':');
         return {'font-family': this.toReadable(t[0]), 'font-weight': (t[1] || 400)};
       };
-      
+
       Fontselect.prototype.getVisibleFonts = function(){
-      
+
         if(this.$results.is(':hidden')) return;
-        
+
         var fs = this;
         var top = this.$results.scrollTop();
         var bottom = top + this.$results.height();
-        
+
         if(this.options.lookahead){
           var li = $('li', this.$results).first().height();
           bottom += li*this.options.lookahead;
         }
-       
+
         $('li', this.$results).each(function(){
 
           var ft = $(this).position().top+top;
@@ -382,26 +382,26 @@ $(document).on('turbolinks:load', function() {
             var font = $(this).data('value');
             fs.addFontLink(font);
           }
-          
+
         });
       };
-      
+
       Fontselect.prototype.addFontLink = function(font){
-      
+
         var link = this.options.api + font;
-      
+
         if ($("link[href*='" + font + "']").length === 0){
 			$('link:last').after('<link href="' + link + '" rel="stylesheet" type="text/css">');
 		}
       };
-    
+
       return Fontselect;
     })();
 
-    return this.each(function(options) {        
+    return this.each(function(options) {
       // If options exist, lets merge them
       if (options) $.extend( settings, options );
-      
+
       return new Fontselect(this, settings);
     });
 

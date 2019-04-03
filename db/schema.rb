6446a2a4ce9f150df2460912e34c4a8e4a99b2e4
kellyChex <kelly@notch8.com>
2019-04-03 10:58:44 -0700

    Add logo and default collection image migrations

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180524010247) do
+ActiveRecord::Schema.define(version: 20190320203028) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -530,6 +530,9 @@ ActiveRecord::Schema.define(version: 20180524010247) do
     t.string "institution_name"
     t.string "institution_name_full"
     t.string "banner_image"
+    t.string "logo_image"
+    t.string "default_collection_image"
+    t.string "default_work_image"
   end
 
   create_table "subject_local_authority_entries", id: :serial, force: :cascade do |t|

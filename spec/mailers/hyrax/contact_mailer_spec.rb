5e10e27639c312cb90821a69313ef9b7b99e53be
Rob Kaufman <rob@notch8.com>
2020-07-30 00:32:18 -0700

    rubocop fixes

--- spec/mailers/hyrax/contact_mailer_spec.rb
+++ spec/mailers/hyrax/contact_mailer_spec.rb
@@ -4,13 +4,14 @@ require 'rails_helper'
 
 RSpec.describe Hyrax::ContactMailer, type: :mailer do
   let(:contact_form) do
-    Hyrax::ContactForm.new(email: 'test@examp
-}  e.com',
-                           category: 'Test',
-                           subject: 'Test',
-                           name: 'Test Tester',
-       
-end                  message: 'This is a test') end
+    Hyrax::ContactForm.new(
+      email: 'test@example.com',
+      category: 'Test',
+      subject: 'Test',
+      name: 'Test Tester',
+      message: 'This is a test'
+    )
+  end
 
   describe "reset_password_instructions" do
     let(:mail) { described_class.contact(contact_form) }

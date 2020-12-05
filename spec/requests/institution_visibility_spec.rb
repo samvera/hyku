feb845043a9e379bad76a4c99e9b05b51885849e
Rob Kaufman <rob@notch8.com>
2020-12-05 00:10:01 -0800

    Add a role to all users if they register directly ot a tenant

--- spec/requests/institution_visibility_spec.rb
+++ spec/requests/institution_visibility_spec.rb
@@ -3,8 +3,8 @@
 RSpec.describe 'Insitution visiblity work access', type: :request, clean: true, multitenant: true do
   let(:account) { create(:account) }
   let(:account2) { create(:account) }
-  let(:tenant_user) { create(:user) }
-  let(:tenant2_user) { create(:user) }
+  let(:tenant_user_attributes) { attributes_for(:user) }
+  let(:tenant2_user_attributes) { attributes_for(:user) }
   let(:work) { create(:work, visibility: 'authenticated') }
 
   before do
@@ -12,57 +12,64 @@ RSpec.describe 'Insitution visiblity work access', type: :request, clean: true, 
     Apartment::Tenant.create(account.tenant)
     Apartment::Tenant.switch(account.tenant) do
       Site.update(account: account)
-      tenant_user
       work
     end
     Apartment::Tenant.create(account2.tenant)
     Apartment::Tenant.switch(account2.tenant) do
       Site.update(account: account2)
-      tenant2_user
     end
+
+    # sign up user 1 at account 1
+    post "http://#{account.cname}/users", params: { user: {
+      display_name: tenant_user_attributes[:name],
+      email: tenant_user_attributes[:email],
+      password: tenant_user_attributes[:password],
+      password_confirmation: tenant_user_attributes[:password]
+    }}
+    @tenant_user = User.last
+    # sign up user 2 at account 2
+    post "http://#{account2.cname}/users", params: { user: {
+      display_name: tenant2_user_attributes[:name],
+      email: tenant2_user_attributes[:email],
+      password: tenant2_user_attributes[:password],
+      password_confirmation: tenant2_user_attributes[:password]
+    }}
+    @tenant2_user = User.last
+    expect(@tenant_user).to_not eq(@tenant2_user)
   end
 
   after do
     WebMock.enable!
     Apartment::Tenant.drop(account.tenant)
+    Apartment::Tenant.drop(account2.tenant)
   end
 
   describe 'as an end-user' do
     it 'allows access for users of the tenant' do
-      login_as tenant_user, scope: :user
+      login_as @tenant_user, scope: :user
       get "http://#{account.cname}/concern/generic_works/#{work.id}"
       expect(response.status).to eq(200)
     end
 
     it 'does not allow access for users of other tenants' do
-      login_as tenant2_user, scope: :user
+      login_as @tenant2_user, scope: :user
       get "http://#{account.cname}/concern/generic_works/#{work.id}"
       expect(response.status).to eq(401)
     end
   end
 
-  describe 'as a user with a role' do
-    let(:tenant_user) do
-      u = create(:user)
-      u.add_role(:depositor)
-      u
-    end
-    let(:tenant2_user) do
-      u = create(:user)
-      u.add_role(:depositor)
-      u
+  describe 'when a user is invited' do
+    before do
+      Apartment::Tenant.switch(account.tenant) do
+        site = Site.instance
+        @tenant2_user.add_role('manager', site)
+      end
     end
 
-    it 'allows access for users of the tenant' do
-      login_as tenant_user, scope: :user
+    it 'now allows access for users of the tenant' do
+      login_as @tenant2_user, scope: :user
       get "http://#{account.cname}/concern/generic_works/#{work.id}"
       expect(response.status).to eq(200)
-    end
-
-    it 'does not allow access for users of other tenants' do
-      login_as tenant2_user, scope: :user
-      get "http://#{account.cname}/concern/generic_works/#{work.id}"
-      expect(response.status).to eq(401)
     end
   end
 end

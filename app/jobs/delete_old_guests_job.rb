1d726514bf620d648e4f9cf488581339a134374f
Rob Kaufman <rob@notch8.com>
2021-07-01 23:30:16 -0700

    rubocop

--- app/jobs/delete_old_guests_job.rb
+++ app/jobs/delete_old_guests_job.rb
@@ -2,17 +2,17 @@
 
 class DeleteOldGuestsJob < ApplicationJob
   non_tenant_job
-  after_perform do |job|
+  after_perform do |_job|
     reenqueue
   end
 
   def perform
-    User.where("guest = ? and updated_at < ?", true, Time.now - 7.days).each { |x| x.destroy}
+    User.where("guest = ? and updated_at < ?", true, Time.current - 7.days).each(&:destroy)
   end
 
   private
 
-  def reenqueue
-    DeleteOldGuestsJob.set(wait_until: Date.tomorrow.midnight).perform_later
-  end
+    def reenqueue
+      DeleteOldGuestsJob.set(wait_until: Date.tomorrow.midnight).perform_later
+    end
 end

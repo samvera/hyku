fa14e783b36002e3d3df483c1390cde8a6aed1a5
Rob Kaufman <rob@notch8.com>
2021-07-15 11:20:47 -0700

    spec cleanup

--- spec/models/domain_name_spec.rb
+++ spec/models/domain_name_spec.rb
@@ -4,7 +4,7 @@ require 'rails_helper'
 
 RSpec.describe DomainName, type: :model do
   it "can provide a canonical cname" do
-    d = DomainNmae.new(cname: 'ThisIsSomeFunkyCaps.com')
+    d = DomainName.new(cname: 'ThisIsSomeFunkyCaps.com')
     expect(d.canonical_cname).to eq('thisissomefunkycaps.com')
   end
 end

464b8c960002b658544c6fd5219d0d6206cda3bd
erik-engstrom <83615707+erik-engstrom@users.noreply.github.com>
2021-08-06 16:10:54 -0700

    Update invitations_controller.rb

--- app/controllers/hyku/invitations_controller.rb
+++ app/controllers/hyku/invitations_controller.rb
@@ -1,8 +1,7 @@
 # frozen_string_literal: true
 
 module Hyku
-  class InvitationsController < Devise
-    class InvitationsController
+  class InvitationsController < Devise::InvitationsController
       # For devise_invitable, specify post-invite path to be 'Manage Users' form
       # (as the user invitation form is also on that page)
       def after_invite_path_for(_resource)

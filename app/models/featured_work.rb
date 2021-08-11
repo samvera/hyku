f4314b39c3a045479fac600ce7421b58947538d2
erik-engstrom <eekareeka@yahoo.com>
2021-08-11 13:06:28 -0600

    rubocop fixes

--- app/models/featured_work.rb
+++ app/models/featured_work.rb
@@ -1,23 +1,22 @@
 # frozen_string_literal: true
 
-class FeaturedWork < ActiveRecord::Base
-    FEATURE_LIMIT = 6
-    validate :count_within_limit, on: :create
-    validates :order, inclusion: { in: proc { 0..FEATURE_LIMIT } }
+class FeaturedWork < ActiveRecord::Base # rubocop:disable Rails/ApplicationRecord
+  FEATURE_LIMIT = 6
+  validate :count_within_limit, on: :create
+  validates :order, inclusion: { in: proc { 0..FEATURE_LIMIT } }
 
-    default_scope { order(:order) }
+  default_scope { order(:order) }
 
-    def count_within_limit
-      return if FeaturedWork.can_create_another?
-      errors.add(:base, "Limited to #{FEATURE_LIMIT} featured works.")
-    end
+  def count_within_limit
+    return if FeaturedWork.can_create_another?
+    errors.add(:base, "Limited to #{FEATURE_LIMIT} featured works.")
+  end
 
-    attr_accessor :presenter
+  attr_accessor :presenter
 
-    class << self
-      def can_create_another?
-        FeaturedWork.count < FEATURE_LIMIT
-      end
+  class << self
+    def can_create_another?
+      FeaturedWork.count < FEATURE_LIMIT
     end
   end
 end

f84106f3d9ab2e4bdf91162577ba24b7099a51c0
edward <bauchiroad@gmail.com>
2021-10-18 18:20:59 +0100

    refactor generate_work_url helper method

--- app/helpers/shared_search_helper.rb
+++ app/helpers/shared_search_helper.rb
@@ -1,8 +1,8 @@
 # frozen_string_literal: true
 
 module SharedSearchHelper
-  # rubocop:disable Metrics/PerceivedComplexity,Metrics/CyclomaticComplexity
   def generate_work_url(model, request)
+    # needed because some attributes eg id is a symbol 7 others are string
     model = model.with_indifferent_access
     request_protocol = request.protocol || 'http://'
     request_port = request.port
@@ -12,14 +12,19 @@ module SharedSearchHelper
     account_cname = cname.class == Array ? cname.try(:first) : cname
 
     has_model = model["has_model_ssim"].first.underscore.pluralize
-    # returns a symbol not a string
-    id = model[:id]
+    id = model["id"]
 
-    if Rails.env.development? || Rails.env.test?
-      "#{request_protocol}#{account_cname || request_host}:#{request_port}/concern/#{has_model}/#{id}"
-    else
-      "#{request_protocol}#{account_cname || request_host}/concern/#{has_model}/#{id}"
-    end
+    request_params = { request_protocol: request_protocol, request_host: request_host, request_port: request_port }
+    get_url(id: id, request: request_params, account_cname: account_cname, has_model: has_model)
   end
-  # rubocop:enable Metrics/PerceivedComplexity,Metrics/CyclomaticComplexity
+
+  private
+
+    def get_url(id:, request:, account_cname:, has_model:)
+      if Rails.env.development? || Rails.env.test?
+        "#{request[:request_protocol]}#{account_cname || request[:request_host]}:#{request[:request_port]}/concern/#{has_model}/#{id}"
+      else
+        "#{request[:request_protocol]}#{account_cname || request[:request_host]}/concern/#{has_model}/#{id}"
+      end
+    end
 end

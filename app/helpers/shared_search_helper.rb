77cf0f85e2040ef0b84bdb63cba7920844cf87c2
edward <bauchiroad@gmail.com>
2021-10-19 18:07:55 +0100

    refactor generate_work_url helper method

--- app/helpers/shared_search_helper.rb
+++ app/helpers/shared_search_helper.rb
@@ -4,17 +4,14 @@ module SharedSearchHelper
   def generate_work_url(model, request)
     # needed because some attributes eg id is a symbol 7 others are string
     model = model.with_indifferent_access
-    request_protocol = request.protocol || 'http://'
-    request_port = request.port
-    request_host = request.host
 
     cname = model["account_cname_tesim"]
-    account_cname = cname.class == Array ? cname.try(:first) : cname
+    account_cname = Array.wrap(cname).first
 
     has_model = model["has_model_ssim"].first.underscore.pluralize
     id = model["id"]
 
-    request_params = { request_protocol: request_protocol, request_host: request_host, request_port: request_port }
+    request_params = %i[protocol host port].map { |method| ["request_#{method}".to_sym, request.send(method)] }.to_h
     get_url(id: id, request: request_params, account_cname: account_cname, has_model: has_model)
   end
 

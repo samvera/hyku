3ba4cacecf7db66b0c5ac192e5107577bccb8c28
Rob Kaufman <rob@notch8.com>
2022-01-02 14:15:05 -0800

    fix setting fallback fedora url

--- app/models/fcrepo_endpoint.rb
+++ app/models/fcrepo_endpoint.rb
@@ -4,7 +4,7 @@ class FcrepoEndpoint < Endpoint
   store :options, accessors: %i[url base_path]
 
   def url
-    self[:url] || ActiveFedora.config&.credentials&.[](:url)
+    options&.[](:url) || ActiveFedora.config&.credentials&.[](:url)
   end
 
   def switch!

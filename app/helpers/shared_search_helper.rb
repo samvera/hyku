7fed75eccebe8b272c95ec96b141934883cc90bb
Rob Kaufman <rob@notch8.com>
2022-05-12 10:23:41 -0700

    Update app/helpers/shared_search_helper.rb
    Co-authored-by: Shana Moore <shana.lavina.moore@gmail.com>

--- app/helpers/shared_search_helper.rb
+++ app/helpers/shared_search_helper.rb
@@ -2,7 +2,7 @@
 
 module SharedSearchHelper
   def generate_work_url(model, request)
-    return model if current_account.search_only?
+    return model unless current_account.search_only?
     # needed because some attributes eg id is a symbol 7 others are string
     model = model.to_h.with_indifferent_access
 

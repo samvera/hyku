1cbcdc919b0e11918b342341e3d5e3112a9fd835
Shana <shana.lavina.moore@gmail.com>
2022-09-23 14:41:38 -0700

    fix rubocop

--- app/helpers/blacklight/url_helper_behavior_decorator.rb
+++ app/helpers/blacklight/url_helper_behavior_decorator.rb
@@ -4,13 +4,13 @@
 module Blacklight
   module UrlHelperBehaviorDecorator
     # override to use generate_work_url
-    def link_to_document(doc, field_or_opts = nil, opts={:counter => nil})
+    def link_to_document(doc, field_or_opts = nil, opts = { counter: nil })
       if field_or_opts.is_a? Hash
         opts = field_or_opts
       else
         field = field_or_opts
       end
-  
+
       field ||= document_show_link_field(doc)
       label = index_presenter(doc).label field, opts
       link_to label, generate_work_url(doc, request), document_link_params(doc, opts)
@@ -20,7 +20,7 @@ module Blacklight
     # see https://playbook-staging.notch8.com/en/samvera/hyku/troubleshooting/multi-tenancy-and-full-urls
     # If we need to preserve the link jacking for tracking, then we need to also amend
     # method `session_tracking_params`so that instead of a path we have a URL
-    def document_link_params(doc, opts)
+    def document_link_params(_doc, opts)
       opts.except(:label, :counter)
     end
   end

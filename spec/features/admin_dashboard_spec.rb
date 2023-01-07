e2acf3f5ca1200c94498fd86a734647e6a53042e
Benjamin Kiah Stroud <32469930+bkiahstroud@users.noreply.github.com>
2023-01-06 17:23:46 -0800

    fix admin dashboard feature spec

--- spec/features/admin_dashboard_spec.rb
+++ spec/features/admin_dashboard_spec.rb
@@ -49,10 +49,10 @@ RSpec.describe 'Admin Dashboard', type: :feature, js: true, clean: true do
 
     it 'shows the status page' do
       visit status_path
-      expect(page).to have_content("Fedora OK")
-      expect(page).to have_content("Solr OK")
-      expect(page).to have_content("Redis OK")
-      expect(page).to have_content("Database OK")
+      expect(page).to have_content("Fedora\nOK")
+      expect(page).to have_content("Solr\nOK")
+      expect(page).to have_content("Redis\nOK")
+      expect(page).to have_content("Database\nOK")
     end
 
     it 'displays the add-users-to-groups page without the hidden form field', js: true do

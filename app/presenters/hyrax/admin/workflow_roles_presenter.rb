524d348231fb0c80cb070f5c72528215184f4701
April Rieger <april.rieger@yahoo.com>
2023-01-25 13:27:47 -0800

    Eh putting this back

--- app/presenters/hyrax/admin/workflow_roles_presenter.rb
+++ app/presenters/hyrax/admin/workflow_roles_presenter.rb
@@ -1,13 +1,58 @@
 # frozen_string_literal: true
 
-# OVERRIDE FILE from Hyrax v3.4.2
-require_dependency Hyrax::Engine.root.join('app', 'presenters', 'hyrax', 'admin', 'workflow_roles_presenter').to_s
+# OVERRIDE FILE from Hyrax v3.4.2 to add groups to workflow roles
+module Hyrax
+  module Admin
+    # Displays a list of users and their associated workflow roles
+    class WorkflowRolesPresenter
+      def users
+        ::User.registered
+      end
 
-Hyrax::Admin::WorkflowRolesPresenter.class_eval do
-  # OVERRIDE: Add new method to add new method to add groups
-  def group_presenter_for(group)
-    agent = group.to_sipity_agent
-    return unless agent
-    AgentPresenter.new(agent)
+      # Override fto add new method to add groups
+      def groups
+        Hyrax::Group.all
+      end
+
+      # Override to add new method to add groups
+      def group_presenter_for(group)
+        agent = group.to_sipity_agent
+        return unless agent
+        AgentPresenter.new(agent)
+      end
+
+      def presenter_for(user)
+        agent = user.sipity_agent
+        return unless agent
+        AgentPresenter.new(agent)
+      end
+
+      class AgentPresenter
+        def initialize(agent)
+          @agent = agent
+        end
+
+        def responsibilities_present?
+          @agent.workflow_responsibilities.any?
+        end
+
+        def responsibilities
+          @agent.workflow_responsibilities.each do |responsibility|
+            yield ResponsibilityPresenter.new(responsibility)
+          end
+        end
+      end
+
+      class ResponsibilityPresenter
+        def initialize(responsibility)
+          @responsibility = responsibility
+          @workflow_role_presenter = WorkflowRolePresenter.new(responsibility.workflow_role)
+        end
+
+        attr_accessor :responsibility
+
+        delegate :label, to: :@workflow_role_presenter
+      end
+    end
   end
 end

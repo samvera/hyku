b05bcdae7dfdd846ec9e28fec4133e2c24f0b611
braydonjustice <braydonjustice@gmail.com>
2023-04-10 13:35:03 -0700

    updates for file urls

--- lib/oai/provider/metadata_format/hyku_dublin_core.rb
+++ lib/oai/provider/metadata_format/hyku_dublin_core.rb
@@ -27,6 +27,8 @@ module OAI
           xml.tag!(prefix.to_s) do
             fields.each do |field|
               values = value_for(field, record, map)
+              next if values.blank?
+
               if values.respond_to?(:each)
                 values.each do |value|
                   xml.tag! field.to_s, value
@@ -35,8 +37,29 @@ module OAI
                 xml.tag! field.to_s, values
               end
             end
+            add_public_file_urls(xml, record)
           end
           xml.target!
+        end
+
+        def add_public_file_urls(xml, record)
+          return if record[:file_set_ids_ssim].blank?
+
+          fs_ids = record[:file_set_ids_ssim].join('" OR "')
+          public_fs_ids = ActiveFedora::SolrService.query(
+              "id:(\"#{fs_ids}\") AND " \
+            "has_model_ssim:FileSet AND " \
+            "visibility_ssi:#{Hydra::AccessControls::AccessRight::VISIBILITY_TEXT_VALUE_PUBLIC}",
+              fl: ["id"],
+              method: :post,
+              rows: 1024 # maximum
+          )
+          return if public_fs_ids.blank?
+
+          public_fs_ids.each do |fs_id_hash|
+            file_download_path = "https://#{Site.instance.account.cname}/downloads/#{fs_id_hash['id']}"
+            xml.tag! 'file_url', file_download_path
+          end
         end
 
         def header_specification

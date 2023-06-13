3b7ff8a9d6791e5dd51a205dcfdc354aced7d9c9
Summer Cook <summer@scientist.com>
2023-06-13 09:09:01 -0500

    revert default colors

--- app/forms/hyrax/forms/admin/appearance.rb
+++ app/forms/hyrax/forms/admin/appearance.rb
@@ -23,28 +23,24 @@ module Hyrax
         }.freeze
 
         DEFAULT_COLORS = {
-          'header_and_footer_background_color' => '#3c3c3c',
-          'header_and_footer_text_color'       => '#dcdcdc',
-          'header_background_color'            => '#222222',
-          'navbar_background_color'            => '#000000',
-          'navbar_link_background_hover_color' => '#ffffff',
-          'navbar_link_text_color'             => '#eeeeee',
-          'navbar_link_text_hover_color'       => '#eeeeee',
-          'link_color'                         => '#2e74b2',
-          'link_hover_color'                   => '#215480',
-          'footer_link_color'                  => '#ffebcd',
-          'footer_link_hover_color'            => '#ffffff',
-          'primary_button_hover_color'         => '#286090',
-          'default_button_background_color'    => '#ffffff',
-          'default_button_border_color'        => '#cccccc',
-          'default_button_text_color'          => '#333333',
-          'active_tabs_background_color'       => '#337ab7',
-          'facet_panel_background_color'       => '#f5f5f5',
-          'facet_panel_text_color'             => '#333333',
-          'collection_banner_text_color'       => '#000000',
-          'searchbar_background_color'         => '#ffffff',
-          'searchbar_background_hover_color'   => '#F3F3F3',
-          'primary_button_background_color'    => '#337ab7'
+          'header_background_color'          => '#3c3c3c',
+          'header_text_color'                => '#dcdcdc',
+          'searchbar_background_color'       => '#000000',
+          'searchbar_background_hover_color' => '#ffffff',
+          'searchbar_text_color'             => '#eeeeee',
+          'searchbar_text_hover_color'       => '#eeeeee',
+          'link_color'                       => '#2e74b2',
+          'link_hover_color'                 => '#215480',
+          'footer_link_color'                => '#ffebcd',
+          'footer_link_hover_color'          => '#ffffff',
+          'primary_button_background_color'  => '#286090',
+          'default_button_background_color'  => '#ffffff',
+          'default_button_border_color'      => '#cccccc',
+          'default_button_text_color'        => '#333333',
+          'active_tabs_background_color'     => '#337ab7',
+          'facet_panel_background_color'     => '#f5f5f5',
+          'facet_panel_text_color'           => '#333333',
+          'collection_banner_text_color'     => '#000000'
         }.freeze
 
         DEFAULT_VALUES = DEFAULT_FONTS.merge(DEFAULT_COLORS).freeze

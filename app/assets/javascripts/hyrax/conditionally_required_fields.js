2eec24b2f95f898842958d0009561042f3a2cb43
Alisha Evans <alishaevn2@gmail.com>
2023-08-25 15:23:44 -0500

    require either rights statement or rights notes be filled out on a generic work form
    we do this by removing the intial requirement on the rights statement input. otherwise, [validateMetatdata](https://github.com/samvera/hyrax/blob/main/app/assets/javascripts/hyrax/save_work/save_work_control.es6\#L148-L155) remained false.
    
    instead, on page load we verify if either field has data. if so, we carry on. if neither does, both are marked as required. from there, if one of the two fields is changed then it is marked as required while the other is removed as required.
    
    if neither field has data and the form attempts to be saved (the save button will NOT be disabled), a popup will show up on the rights statement asking for it to be filled out.
    
    TODO: make this unspecific to the generic work form.

---
+++ app/assets/javascripts/hyrax/conditionally_required_fields.js
@@ -0,0 +1,43 @@
+/**
+ * This file is used to conditionally require fields on the edit/new work forms.
+ * At the moment, rights statement is required by default, and rights notes is not.
+ * One or the other must be filled out in order to create/update a work
+ */
+
+// check the value of the rights statement and rights notes fields on page load
+$(document).on('turbolinks:load', function () {
+  $('#generic_work_rights_statement').each(function () {
+    if (this.value || $('#generic_work_rights_notes')[0].value) return;
+
+    $('#generic_work_rights_statement').attr('required', true);
+    $('#generic_work_rights_notes').attr('required', true);
+  });
+});
+
+// check the value of the rights notes field after its focus is removed
+$(document).on('turbolinks:load', function () {
+  return $('body').on('blur', '#generic_work_rights_notes', function () {
+    if (this.value === undefined) return;
+    ($('#generic_work_rights_notes').attr('required', true));
+
+    if(this.value === '') {
+      ($('#generic_work_rights_statement').attr('required', true))
+    } else {
+      ($('#generic_work_rights_statement').attr('required', false))
+    }
+  });
+});
+
+// check the value of the rights statement field after its focus is removed
+$(document).on('turbolinks:load', function () {
+  return $('body').on('blur', '#generic_work_rights_statement', function () {
+    if (this.value === undefined || !this.value) return;
+    ($('#generic_work_rights_statement').attr('required', true))
+
+    if(this.value === '') {
+      ($('#generic_work_rights_notes').attr('required', true))
+    } else {
+      ($('#generic_work_rights_notes').attr('required', false))
+    }
+  });
+});

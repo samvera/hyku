ff12cecbb81343a2b1ae9175fe4cd134c0df432f
LaRita Robinson <larita@scientist.com>
2023-12-15 15:47:40 -0500

    🧹 Get SitesControllerSpecs working
     File fixtures were broken.
     Ref https://github.com/scientist-softserv/hykuup_knapsack/issues/56

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -104,6 +104,7 @@ NegativeCaptcha.test_mode = true
 RSpec.configure do |config|
   # Remove this line if you're not using ActiveRecord or ActiveRecord fixtures
   config.fixture_path = "#{::Rails.root}/spec/fixtures"
+  config.file_fixture_path = "#{::Rails.root}/spec/fixtures"
 
   # If you're not using ActiveRecord, or you'd prefer not to run each of your
   # examples within a transaction, remove the following line or assign false
@@ -131,7 +132,6 @@ RSpec.configure do |config|
   # config.filter_gems_from_backtrace("gem name")
 
   config.include Devise::Test::ControllerHelpers, type: :controller
-  config.include Fixtures::FixtureFileUpload
   config.include FactoryBot::Syntax::Methods
   config.include ApplicationHelper, type: :view
   config.include Warden::Test::Helpers, type: :feature

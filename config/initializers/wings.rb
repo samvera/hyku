6021bdd354c4c84ba527db480d50f32fbea4ea8c
Kirk Wang <kirk.wang@scientist.com>
2024-01-05 14:22:53 -0800

    I35 valkyrize hyku wings (#2130)
    * 🚧 Get wings set up
    
    This copies over the a modified wings initializer from AMS.  It also
    runs the migrations for the Valkyrie engine.
    
    ```sh
    bundle exec rails valkyrie_engine:install:migrations
    bundle exec rails db:migrate
    ```
    
    * 🚧 Adding temporary hack so we can proceed
    
    The problem we're facing is that the Valkyrie::IndexingAdapter registers
    the Valkyrie::Indexing::Solr::IndexingAdapter as the default adapter,
    however, this does not work in a multi-tenant environment.  The 'core'
    is set to 'hyrax-valkyrie' and it needs to be the tenant's ID.  Also the
    connection url needs the username and password in it.
    
    See:
      - https://github.com/samvera/hyrax/blob/double_combo/lib/valkyrie/indexing/solr/indexing_adapter.rb#L71
      - https://github.com/samvera/hyrax/blob/double_combo/lib/valkyrie/indexing/solr/indexing_adapter.rb#L86
    
    We put in the hack to get us past this issue.
    
    * 🧹 Multi-tenant connection & switching options
    
    ---------
    
    Co-authored-by: LaRita Robinson <larita@scientist.com>

---
+++ config/initializers/wings.rb
@@ -0,0 +1,93 @@
+# frozen_string_literal: true
+# rubocop:disable Metrics/BlockLength
+Rails.application.config.after_initialize do
+  [
+    GenericWork,
+    Image
+  ].each do |klass|
+    Wings::ModelRegistry.register("#{klass}Resource".constantize, klass)
+    # we register itself so we can pre-translate the class in Freyja instead of having to translate in each query_service
+    Wings::ModelRegistry.register(klass, klass)
+  end
+  Wings::ModelRegistry.register(Collection, Collection)
+  Wings::ModelRegistry.register(Hyrax::PcdmCollection, Collection)
+  Wings::ModelRegistry.register(Hyrax::AdministrativeSet, AdminSet)
+  Wings::ModelRegistry.register(AdminSet, AdminSet)
+
+  Valkyrie::MetadataAdapter.register(
+    Freyja::MetadataAdapter.new,
+    :freyja
+  )
+  Valkyrie.config.metadata_adapter = :freyja
+  Hyrax.config.query_index_from_valkyrie = true
+  Hyrax.config.index_adapter = :solr_index
+
+  Valkyrie::StorageAdapter.register(
+    Valkyrie::Storage::Disk.new(base_path: Rails.root.join("storage", "files"),
+                                file_mover: FileUtils.method(:cp)),
+    :disk
+  )
+  Valkyrie.config.storage_adapter  = :disk
+  Valkyrie.config.indexing_adapter = :solr_index
+
+  # load all the sql based custom queries
+  [
+    Hyrax::CustomQueries::Navigators::CollectionMembers,
+    Hyrax::CustomQueries::Navigators::ChildCollectionsNavigator,
+    Hyrax::CustomQueries::Navigators::ParentCollectionsNavigator,
+    Hyrax::CustomQueries::Navigators::ChildFileSetsNavigator,
+    Hyrax::CustomQueries::Navigators::ChildWorksNavigator,
+    Hyrax::CustomQueries::Navigators::FindFiles,
+    Hyrax::CustomQueries::FindAccessControl,
+    Hyrax::CustomQueries::FindCollectionsByType,
+    Hyrax::CustomQueries::FindFileMetadata,
+    Hyrax::CustomQueries::FindIdsByModel,
+    Hyrax::CustomQueries::FindManyByAlternateIds,
+    Hyrax::CustomQueries::FindModelsByAccess,
+    Hyrax::CustomQueries::FindCountBy,
+    Hyrax::CustomQueries::FindByDateRange,
+    Hyrax::CustomQueries::FindByBulkraxIdentifier
+  ].each do |handler|
+    Hyrax.query_service.services[0].custom_queries.register_query_handler(handler)
+  end
+
+  [
+    Wings::CustomQueries::FindByBulkraxIdentifier
+  ].each do |handler|
+    Hyrax.query_service.services[1].custom_queries.register_query_handler(handler)
+  end
+
+  Wings::ModelRegistry.register(GenericWorkResource, GenericWork)
+  Wings::ModelRegistry.register(ImageResource, Image)
+end
+# rubocop:enable Metrics/BlockLength
+
+Rails.application.config.to_prepare do
+  Hyrax::AdministrativeSet.class_eval do
+    attribute :internal_resource, Valkyrie::Types::Any.default("AdminSet"), internal: true
+  end
+
+  Hyrax::PcdmCollection.class_eval do
+    attribute :internal_resource, Valkyrie::Types::Any.default("Collection"), internal: true
+  end
+
+  Hyrax::FileSet.class_eval do
+    attribute :internal_resource, Valkyrie::Types::Any.default("FileSet"), internal: true
+  end
+
+  Valkyrie.config.resource_class_resolver = lambda do |resource_klass_name|
+    klass_name = resource_klass_name.gsub(/Resource$/, '')
+    if %w[
+      GenericWork
+      Image
+    ].include?(klass_name)
+      "#{klass_name}Resource".constantize
+    elsif 'Collection' == klass_name
+      Hyrax::PcdmCollection
+    elsif 'AdminSet' == klass_name
+      Hyrax::AdministrativeSet
+    else
+      klass_name.constantize
+    end
+  end
+end

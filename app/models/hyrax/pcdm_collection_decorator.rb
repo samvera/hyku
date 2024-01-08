a1df23e2c949d8e78822c10c8d0916db79f196e2
LaRita Robinson <larita@scientist.com>
2024-01-08 18:37:19 -0500

    🚧 WIP - Collections & Administrative Sets
    The first pieces of work to get Collections working.

--- app/models/hyrax/pcdm_collection_decorator.rb
+++ app/models/hyrax/pcdm_collection_decorator.rb
@@ -3,4 +3,13 @@
 Hyrax::PcdmCollection.class_eval do
   include Hyrax::Schema(:basic_metadata)
   include Hyrax::ArResource
+  prepend OrderAlready.for(:creator)
+
+  ## TODO: Custom behavior to make functional
+  # after_update :remove_featured, if: proc { |collection| collection.private? }
+  # after_destroy :remove_featured
+
+  # def remove_featured
+  #   FeaturedCollection.where(collection_id: id).destroy_all
+  # end
 end

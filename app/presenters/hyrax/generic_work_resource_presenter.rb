1ff3036c3897c2c5c43dc84bef9e2992030fc7ab
Kirk Wang <kirk.wang@scientist.com>
2024-01-09 13:34:01 -0800

    🧹 Add custom presenters for resource work types
    The Hyrax generator does not generate presenters for resource work
    types so I'm adding them here and ensuring the respective resource work
    type controllers use them.

---
+++ app/presenters/hyrax/generic_work_resource_presenter.rb
@@ -0,0 +1,6 @@
+# frozen_string_literal: true
+
+module Hyrax
+  class GenericWorkResourcePresenter < Hyku::WorkShowPresenter
+  end
+end
---
+++ app/presenters/hyrax/image_resource_presenter.rb
@@ -0,0 +1,6 @@
+# frozen_string_literal: true
+
+module Hyrax
+  class ImageResourcePresenter < Hyku::WorkShowPresenter
+  end
+end

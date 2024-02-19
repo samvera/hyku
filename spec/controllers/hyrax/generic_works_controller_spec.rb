d9dccb4cad01f58d61b5b62cdb44eb2f99c3f6c2
Jeremy Friesen <jeremy.n.friesen@gmail.com>
2024-02-19 13:03:40 -0500

    ☑️ Fixing tests squashing bugs that might show up

--- spec/controllers/hyrax/generic_works_controller_spec.rb
+++ spec/controllers/hyrax/generic_works_controller_spec.rb
@@ -4,14 +4,8 @@ require 'rails_helper'
 
 RSpec.describe Hyrax::GenericWorksController do
   let(:user) { FactoryBot.create(:user) }
-  let(:work) { FactoryBot.create(:work_with_one_file, user:) }
-  let(:file_set) { work.ordered_members.to_a.first }
-
-  before do
-    Hydra::Works::AddFileToFileSet.call(file_set,
-                                        fixture_file_upload('images/world.png'),
-                                        :original_file)
-  end
+  let(:work) { FactoryBot.valkyrie_create(:generic_work_resource, :with_one_file_set, depositor: user.user_key) }
+  let(:file_set) { Hyrax.query_service.find_members(resource: work).first }
 
   it "includes Hyrax::IiifAv::ControllerBehavior" do
     expect(described_class.include?(Hyrax::IiifAv::ControllerBehavior)).to be true
@@ -20,7 +14,7 @@ RSpec.describe Hyrax::GenericWorksController do
   describe "#presenter" do
     subject { controller.send :presenter }
 
-    let(:solr_document) { SolrDocument.new(FactoryBot.create(:generic_work).to_solr) }
+    let(:solr_document) { SolrDocument.new(work.to_solr) }
 
     before do
       allow(controller).to receive(:search_result_document).and_return(solr_document)
@@ -29,6 +23,8 @@ RSpec.describe Hyrax::GenericWorksController do
     it "initializes a presenter" do
       expect(subject).to be_kind_of Hyku::WorkShowPresenter
       expect(subject.manifest_url).to eq "http://test.host/concern/generic_works/#{solr_document.id}/manifest"
+
+      get :manifest, params: { id: solr_document.id }
     end
   end
 end
--- spec/factories/generic_work_resource.rb
+++ spec/factories/generic_work_resource.rb
@@ -1,5 +1,12 @@
 # frozen_string_literal: true
 
 FactoryBot.define do
-  factory :generic_work_resource, parent: :hyrax_work, class: 'GenericWorkResource'
+  factory :generic_work_resource, parent: :hyrax_work, class: 'GenericWorkResource' do
+    identifier do
+      %w[
+        ISBN:978-83-7659-303-6 978-3-540-49698-4 9790879392788
+        doi:10.1038/nphys1170 3-921099-34-X 3-540-49698-x 0-19-852663-6
+      ]
+    end
+  end
 end

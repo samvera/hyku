f59f0702235186f33b95deb520eb35359c493cf4
Jeremy Friesen <jeremy.n.friesen@gmail.com>
2024-02-28 11:25:09 -0500

    Remove transient as the underlying problem fixed

--- spec/factories/admin_set_resources.rb
+++ spec/factories/admin_set_resources.rb
@@ -3,12 +3,6 @@
 # Extending https://github.com/samvera/hyrax/blob/main/spec/factories/administrative_sets.rb
 FactoryBot.modify do
   factory :hyrax_admin_set do
-    transient do
-      # We need FactoryBot declaration, otherwise we use the same strategy
-      # (e.g. FactoryBot.valkyrie_create), and stumble into a nightmare.
-      user { FactoryBot.create(:user) }
-    end
-
     after(:create) do |admin_set, evaluator|
       if evaluator.with_permission_template&.is_a?(Hash) &&
          (evaluator.with_permission_template[:with_workflows] ||
@@ -30,11 +24,6 @@ FactoryBot.define do
   # ```ruby
   # FactoryBot.valkyrie_create(:hyku_admin_set, with_permission_template: true)
   # ```
-  factory :hyku_admin_set, parent: :hyrax_admin_set, class: Hyrax.config.admin_set_model do
-    transient do
-      # We need FactoryBot declaration, otherwise we use the same strategy
-      # (e.g. FactoryBot.valkyrie_create), and stumble into a nightmare.
-      user { FactoryBot.create(:user) }
-    end
+  factory :hyku_admin_set, parent: :hyrax_admin_set, class: AdminSetResource do
   end
 end

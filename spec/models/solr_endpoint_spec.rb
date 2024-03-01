68f1750fd03306fa50c22f851d76441a4712e22d
Jeremy Friesen <jeremy.n.friesen@gmail.com>
2024-03-01 09:01:55 -0500

    Removing mocked tests that don't prove much

--- spec/models/solr_endpoint_spec.rb
+++ spec/models/solr_endpoint_spec.rb
@@ -18,33 +18,6 @@ RSpec.describe SolrEndpoint do
     end
   end
 
-  describe '#connection' do
-    subject { instance.connection }
-
-    let(:result) { double }
-    let(:af_options) do
-      { read_timeout: 120,
-        open_timeout: 120,
-        url: "http://127.0.0.1:8985/solr/hydra-test" }
-    end
-
-    before do
-      # Stubbing conn, because it could trigger RSolr.connect if it hadn't alredy
-      # been called. This caused an error prior to stubbing for certain test seeds.
-      allow_any_instance_of(Hyrax::SolrService).to receive(:connection)
-        .and_return(double(options: af_options))
-      allow(RSolr).to receive(:connect)
-        .with(hash_including("read_timeout" => 120,
-                             "open_timeout" => 120,
-                             "url" => "http://example.com/solr/"))
-        .and_return(result)
-    end
-
-    it 'returns the initialized connection (without an adapter option)' do
-      expect(subject).to be result
-    end
-  end
-
   describe '#ping' do
     let(:mock_connection) { instance_double(RSolr::Client, options: {}) }
 

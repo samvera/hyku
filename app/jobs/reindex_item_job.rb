d3df1e099e3d46d70ad8e574590eb57e939c59f9
LaRita Robinson <laritakr@users.noreply.github.com>
2024-04-01 12:29:58 -0400

    🎁 Valkyrize Reindex jobs (#2173)
    Addresses all reindex jobs to ensure that they will work for
    either valkyrie or active fedora objects.
    
    Ensures that works and collections are appropriately reindexed as
    default work and collection images are added OR removed (previously,
    reindexing didn't occur with removal of default images, resulting in
    works displaying with a missing image.)

--- app/jobs/reindex_item_job.rb
+++ app/jobs/reindex_item_job.rb
@@ -1,7 +1,9 @@
 # frozen_string_literal: true
 
 class ReindexItemJob < ApplicationJob
-  def perform(item)
+  def perform(item_id)
+    item = Hyrax.query_service.find_by(id: item_id)
+
     if item.is_a?(Valkyrie::Resource)
       Hyrax.index_adapter.save(resource: item)
     else

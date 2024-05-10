1a3bda01c7f3bd3ef5e6b827df0285ec8aa9c6ea
Shana Moore <shana@scientist.com>
2024-05-10 11:32:08 -0700

    TODO: test via UI
    Contributes back cross search host link in the splash search form

--- config/application.rb
+++ config/application.rb
@@ -185,6 +185,29 @@ module Hyku
       end
     end
 
+    ##
+    # @return [String] the URL to use for searching across all tenants.
+    #
+    # @see .cross_tenant_search_host
+    # @see https://github.com/scientist-softserv/palni-palci/issues/947
+    def self.cross_tenant_search_url
+      # Do not include the scheme (e.g. http or https) but instead let the browser apply the correct
+      # scheme.
+      "//#{cross_tenant_search_host}/catalog"
+    end
+
+    ##
+    # @api private
+    #
+    # @return [String] the host (e.g. "search.hykucommons.org") for cross-tenant search.
+    def self.cross_tenant_search_host
+      # I'm providing quite a few fallbacks, as this URL is used on the first page you'll see in a
+      # new Hyku instance.
+      ENV["HYKU_CROSS_TENANT_SEARCH_HOST"].presence ||
+      Account.where(search_only: true).limit(1).pluck(:cname)&.first ||
+      "HYKU_CROSS_TENANT_SEARCH_HOST not set"
+    end
+
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.

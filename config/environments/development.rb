4bce0e7a4e3a5153731515909b0afa39b826a26d
Shana Moore <shana@scientist.com>
2024-07-09 15:25:28 -0700

    :gift: add Hyrax::Administrative set to m3_profile and update hyrax

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -12,6 +12,7 @@ Rails.application.configure do
                          else
                            false
                          end
+  config.reload_classes_only_on_change = false
 
   # Do not eager load code on boot.
   config.eager_load = false

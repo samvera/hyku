d5c65e815e743ce199bd97a2be3abca6c068d581
LaRita Robinson <larita@scientist.com>
2024-08-30 20:01:33 -0400

    Finish Migration job
    Fixes migrate resources job
    Adds spec for job

--- app/jobs/migrate_resources_job.rb
+++ app/jobs/migrate_resources_job.rb
@@ -3,28 +3,29 @@
 # migrates models from AF to valkyrie
 class MigrateResourcesJob < ApplicationJob
   # input [Array>>String] Array of ActiveFedora model names to migrate to valkyrie objects
-  # defaults to AdminSet & Collection models
+  # defaults to AdminSet & Collection models if empty
   def perform(models: [])
     models = collection_models_list if models.empty?
 
     models.each do |model|
-      resources = Hyrax.query_service.find_all_of_model(model:)
-      resources.each do |res|
+      model.constantize.find_each do |item|
+        res = Hyrax.query_service.find_by(id: item.id)
+        puts "🦋🦋 Migrating #{model} #{res.title.first}, id: #{res.id.to_s} 🦋🦋"
         # start with a form for the resource
-        fm = form_for(model:).constantize.new(resource: as)
+        fm = form_for(model:).constantize.new(resource: res)
         # save the form
         converted = Hyrax.persister.save(resource: fm)
         # reindex
         Hyrax.index_adapter.save(resource: converted)
       end
     end
+  end
 
-    def form_for(model:)
-      model.to_s + 'ResourceForm'
-    end
+  def form_for(model:)
+    model.to_s + 'ResourceForm'
+  end
 
-    def collection_models_list
-      %w(AdminSet Collection)
-    end
+  def collection_models_list
+    %w(AdminSet Collection)
   end
 end

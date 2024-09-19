20e3904ad76add6e51f145810c90c4d1efc02f4b
Rob Kaufman <rob@notch8.com>
2024-09-19 16:46:48 -0700

    add some docs

--- config/initializers/wings.rb
+++ config/initializers/wings.rb
@@ -42,12 +42,12 @@ Rails.application.config.after_initialize do
     shrine_s3_options = {
       bucket: ENV.fetch("REPOSITORY_S3_BUCKET") { "nurax_pg#{Rails.env}" },
       region: ENV.fetch("REPOSITORY_S3_REGION", "us-east-1"),
-      access_key_id: (ENV["REPOSITORY_S3_ACCESS_KEY"] || ENV["MINIO_ACCESS_KEY"]),
-      secret_access_key: (ENV["REPOSITORY_S3_SECRET_KEY"] || ENV["MINIO_SECRET_KEY"])
+      access_key_id: ENV["REPOSITORY_S3_ACCESS_KEY"],
+      secret_access_key: ENV["REPOSITORY_S3_SECRET_KEY"]
     }
 
-    if ENV["MINIO_ENDPOINT"].present?
-      shrine_s3_options[:endpoint] = "http://#{ENV["MINIO_ENDPOINT"]}:#{ENV.fetch("MINIO_PORT", 9000)}"
+    if ENV["REPOSITORY_S3_ENDPOINT"].present?
+      shrine_s3_options[:endpoint] = "http://#{ENV["REPOSITORY_S3_ENDPOINT"]}:#{ENV.fetch("REPOSITORY_S3_PORT", 9000)}"
       shrine_s3_options[:force_path_style] = true
     end
 

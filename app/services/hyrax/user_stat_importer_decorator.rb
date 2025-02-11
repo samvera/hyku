41eef35c61c7f50d07e5c04f6a08a6e5adbcc77d
Kirk Wang <kirk.wang@scientist.com>
2025-02-11 09:16:30 -0800

    🧹 Remove early return and over add #sorted_users
    This commit will remove the early return in the user_stat_collection_job
    since it was too aggressive and will have a condition where it will
    never run.  Also, adding an override to #sorted_users to acocunt for
    multitenancy in Hyku.

--- app/services/hyrax/user_stat_importer_decorator.rb
+++ app/services/hyrax/user_stat_importer_decorator.rb
@@ -2,9 +2,21 @@
 
 # OVERRIDE Hyrax v5.0.1 to use Hyrax.query_service to find the fileset instead of FileSet.find
 #   Remove this if https://github.com/samvera/hyrax/pull/6992 gets merged and Hyrax is updated appropriately
-
+#   Also override to #sorted_users to account for multitenancy in Hyku
 module Hyrax
   module UserStatImporterDecorator
+    UserRecord = Struct.new('UserRecord', :id, :user_key, :last_stats_update)
+
+    # Returns an array of users of that tenant sorted by the date of their last stats update.
+    # Users that have not been recently updated will be at the top of the array.
+    def sorted_users
+      users = []
+      ::User.registered.for_repository.without_system_accounts.uniq.each do |user|
+        users.push(UserRecord.new(user.id, user.user_key, date_since_last_cache(user)))
+      end
+      users.sort_by(&:last_stats_update)
+    end
+
     private
 
     def process_files(stats, user, start_date)

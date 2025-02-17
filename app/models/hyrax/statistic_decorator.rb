173817081e2e58c82428c04a414f4212d41d8c59
Kirk Wang <kirk.wang@scientist.com>
2025-02-17 08:20:42 -0800

    🧹 Remove override and add another for GA
    This commit will remove the #process_files method override because we
    have it contributed back to Hyrax.  It will also add an override to the
    Hyrax::Statistic to get the correct ga_key to be used to find the
    downloads stat from the Hyrax::Analytics::Result object for the
    FileDownloadStat.

---
+++ app/models/hyrax/statistic_decorator.rb
@@ -0,0 +1,23 @@
+# frozen_string_literal: true
+
+# OVERRIDE Hyrax v5.0.4 to use a different GA key, by default FileDownloadStat uses :totalEvents
+#   however this isn't working for because what we get back from Hyrax::Analytics::Results object
+#   is a hash with :pageviews instead which doesn't work for our needs.
+
+module Hyrax
+  module StatisticDecorator
+    extend ActiveSupport::Concern
+
+    class_methods do
+      private
+
+      def combined_stats(object, start_date, object_method, ga_key, user_id = nil)
+        ga_key = :pageviews if self == FileDownloadStat
+
+        super
+      end
+    end
+  end
+end
+
+Hyrax::Statistic.prepend(Hyrax::StatisticDecorator)
--- app/services/hyrax/user_stat_importer_decorator.rb
+++ app/services/hyrax/user_stat_importer_decorator.rb
@@ -1,8 +1,7 @@
 # frozen_string_literal: true
 
-# OVERRIDE Hyrax v5.0.1 to use Hyrax.query_service to find the fileset instead of FileSet.find
-#   Remove this if https://github.com/samvera/hyrax/pull/6992 gets merged and Hyrax is updated appropriately
-#   Also override to #sorted_users to account for multitenancy in Hyku
+# OVERRIDE Hyrax v5.0.1 to override to #sorted_users to account for multitenancy in Hyku
+
 module Hyrax
   module UserStatImporterDecorator
     UserRecord = Struct.new('UserRecord', :id, :user_key, :last_stats_update)
@@ -15,20 +14,6 @@ module Hyrax
         users.push(UserRecord.new(user.id, user.user_key, date_since_last_cache(user)))
       end
       users.sort_by(&:last_stats_update)
-    end
-
-    private
-
-    def process_files(stats, user, start_date)
-      file_ids_for_user(user).each do |file_id|
-        file = Hyrax.query_service.find_by(id: file_id)
-        view_stats = extract_stats_for(object: file, from: FileViewStat, start_date: start_date, user: user)
-        stats = tally_results(view_stats, :views, stats) if view_stats.present?
-        delay
-        dl_stats = extract_stats_for(object: file, from: FileDownloadStat, start_date: start_date, user: user)
-        stats = tally_results(dl_stats, :downloads, stats) if dl_stats.present?
-        delay
-      end
     end
   end
 end

5747020fa268dbbf3f6582c212713f555e5d2fe9
Shana Moore <shana.lavina.moore@gmail.com>
2025-03-03 08:25:09 -0800

    Delete 20231010123456_remove_derived_from_hyrax_uploaded_file.rb
    this migration should go part of pals

--- db/migrate/20231010123456_remove_derived_from_hyrax_uploaded_file.rb
+++
@@ -1,7 +0,0 @@
-class RemoveDerivedFromHyraxUploadedFile < ActiveRecord::Migration[5.2]
-  def change
-    if column_exists?(:uploaded_files, :derived)
-      remove_column :uploaded_files, :derived
-    end
-  end
-end 

4004e22781ce37117549474cace56466bd40f72e
Rob Kaufman <rob@notch8.com>
2025-04-19 18:39:29 -0700

    fix boolean cast

--- config/initializers/rack_attack.rb
+++ config/initializers/rack_attack.rb
@@ -2,5 +2,5 @@
 # No more that 20 requersts per minitue per ip
 limit = ENV.fetch('HYKU_ATTACK_RATE_LIMIT', 20)
 period = ENV.fetch('HYKU_ATTACK_RATE_PERIOD', 60)
-throttle_off = ENV.fetch('HYKU_ATTACK_RATE_THROTTLE_OFF', Rails.env.development? || Rails.env.test?).to_bool
+throttle_off = ActiveModel::Type::Boolean.new.cast(ENV.fetch('HYKU_ATTACK_RATE_THROTTLE_OFF', Rails.env.development? || Rails.env.test?))
 Rack::Attack.throttle("requests by ip", limit: limit, period: period, &:ip) unless throttle_off

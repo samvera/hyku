02eb6f758d2ff7884fa1b63f9f30bfd85423da25
Shana Moore <shana.lavina.moore@gmail.com>
2025-07-21 08:19:24 -0700

    defaults to en locale
    Issue: https://github.com/notch8/hykuup_knapsack/issues/423
    
    This change will ensure that if a locale is not specified in the URL, it will default to English.

--- config/application.rb
+++ config/application.rb
@@ -291,6 +291,7 @@ module Hyku
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
+    config.i18n.default_locale = :en
 
     # Gzip all responses.  We probably could do this in an upstream proxy, but
     # configuring Nginx on Elastic Beanstalk is a pain.

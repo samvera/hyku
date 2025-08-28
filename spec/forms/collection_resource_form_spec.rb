96e9a95826310404239f4de3bd978de463e71bef
Sarah Proctor <sproctor950@gmail.com>
2025-08-28 13:29:37 -0700

    🎁 Exclude collections from catalog search
    This commit adds a checkbox to the discovery tab of the collection edit
    form that will exclude the collection from the catalog search.
    
    `hide_from_catalog_search` is added as an attribute on collection
    resource.
    
    Ref:
    - https://github.com/notch8/adventist_knapsack/issues/892

--- spec/forms/collection_resource_form_spec.rb
+++ spec/forms/collection_resource_form_spec.rb
@@ -6,8 +6,48 @@ require 'rails_helper'
 require 'valkyrie/specs/shared_specs'
 
 RSpec.describe CollectionResourceForm do
-  let(:change_set) { described_class.new(resource:) }
+  # Valkyrie resource model
   let(:resource)   { CollectionResource.new }
+  # CollectionResourceForm class
+  let(:change_set) { described_class.new(resource:) }
 
   it_behaves_like 'a Valkyrie::ChangeSet'
+
+  describe 'hide_from_catalog_search' do
+    context 'Valkyrie resource model' do
+      # hide_from_catalog_search is an attribute in the collection_resource.yaml
+      it 'is an attribute on the resource' do
+        resource.respond_to?(:hide_from_catalog_search)
+      end
+
+      it 'defaults as nil' do
+        expect(resource.hide_from_catalog_search).to be nil
+      end
+
+      it 'can be set to a boolean value' do
+        resource.hide_from_catalog_search = true
+        expect(resource.hide_from_catalog_search).to be true
+
+        resource.hide_from_catalog_search = false
+        expect(resource.hide_from_catalog_search).to be false
+      end
+    end
+
+    context 'change_set form' do
+      it 'can get and set values' do
+        change_set.respond_to?(:hide_from_catalog_search)
+        change_set.respond_to?(:hide_from_catalog_search=)
+      end
+
+      it 'can be set the value to true when the box is checked' do
+        change_set.hide_from_catalog_search = true
+        expect(change_set.hide_from_catalog_search).to be true
+      end
+
+      it 'can be set the value to false when the box is unchecked' do
+        change_set.hide_from_catalog_search = false
+        expect(change_set.hide_from_catalog_search).to be false
+      end
+    end
+  end
 end

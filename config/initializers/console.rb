5ef80b9268b50d95f2e5dfa16486437a16619468
Rob Kaufman <rob@notch8.com>
2025-09-23 12:27:25 -0700

    appease rubocop

--- config/initializers/console.rb
+++ config/initializers/console.rb
@@ -3,11 +3,11 @@
 Rails.application.console do
   case Account.count
   when 0
-    puts "***** No accounts, found, please run the seeds *****"
+    Rails.logger.error "***** No accounts, found, please run the seeds *****"
   when 1
     switch!(Account.first)
-    puts "***** Only one account found, switching to it automatically *****"
+    Rails.logger.error "***** Only one account found, switching to it automatically *****"
   else
-    puts "***** Multiple accounts found, dont' forget to switch in to one with switch!(ACCOUNT_NAME) or switch!(ACCOUNT_CNAME) *****"
+    Rails.logger.error "***** Multiple accounts found, dont' forget to switch in to one with switch!(ACCOUNT_NAME) or switch!(ACCOUNT_CNAME) *****"
   end
 end

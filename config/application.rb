1f7c71f95ae18d50ad4ee9db214c5c3828ef0a06
Rob Kaufman <rob@notch8.com>
2025-09-25 10:02:21 -0700

    update config and bin files

--- config/application.rb
+++ config/application.rb
@@ -145,6 +145,15 @@ module Hyku
 
   # rubocop:disable Metrics/ClassLength
   class Application < Rails::Application
+    # Initialize configuration defaults for originally generated Rails version.
+    config.load_defaults 7.2
+
+    # Please, add to the `ignore` list any other `lib` subdirectories that do
+    # not contain `.rb` files, or that should not be reloaded or eager loaded.
+    # Common ones are `templates`, `generators`, or `middleware`, for example.
+    config.autoload_lib(ignore: %w[assets tasks])
+    config.add_autoload_paths_to_load_path = true
+
     ##
     # @!group Class Attributes
     #
@@ -359,6 +368,7 @@ module Hyku
         end
       end
 
+      require Rails.root.join('app', 'models', 'concerns', 'account_switch')
       Object.include(AccountSwitch)
     end
 

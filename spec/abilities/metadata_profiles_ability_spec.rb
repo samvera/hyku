3f154ff02056dfd15e9598faa670f3d2c80a5230
Tracy McCormick <tracy.alan.mccormick@gmail.com>
2025-09-29 16:03:38 -0400

    Refactor metadata profiles ability tests

--- spec/abilities/metadata_profiles_ability_spec.rb
+++ spec/abilities/metadata_profiles_ability_spec.rb
@@ -1,53 +1,43 @@
 # frozen_string_literal: true
 
 require 'rails_helper'
+require 'cancan/matchers'
 
 RSpec.describe 'Metadata Profiles Abilities' do
   let(:user) { create(:admin) }
   
   subject { Ability.new(user) }
 
-  context 'with flexible metadata enabled' do
+  context 'for regular (non-search) tenants' do
     before do
-      allow(Hyrax.config).to receive(:flexible?).and_return(true)
+      allow(Site.account).to receive(:search_only?).and_return(false)
     end
 
-    context 'for regular (non-search) tenants' do
-      before do
-        allow(Site.account).to receive(:search_only?).and_return(false)
-      end
-
-      it 'allows admin users to manage metadata profiles' do
-        expect(subject).to be_able_to(:manage, :metadata_profiles)
-      end
-    end
-
-    context 'for search-only tenants' do
-      before do
-        # Mock the account to be search-only
-        search_account = instance_double("Account")
-        allow(search_account).to receive(:search_only?).and_return(true)
-        allow(Site).to receive(:account).and_return(search_account)
-      end
-
-      it 'does not allow users to manage metadata profiles' do
-        expect(subject).not_to be_able_to(:manage, :metadata_profiles)
-        expect(subject).not_to be_able_to(:create, :metadata_profiles)
-        expect(subject).not_to be_able_to(:read, :metadata_profiles)
-        expect(subject).not_to be_able_to(:update, :metadata_profiles)
-      end
+    it 'allows admin users to manage metadata profiles' do
+      is_expected.to be_able_to(:manage, :metadata_profiles)
     end
   end
 
-  context 'with flexible metadata disabled' do
+  context 'for search-only tenants' do
     before do
-      allow(Hyrax.config).to receive(:flexible?).and_return(false)
+      # Mock the account to be search-only
+      search_account = instance_double("Account")
+      allow(search_account).to receive(:search_only?).and_return(true)
+      allow(Site).to receive(:account).and_return(search_account)
     end
 
-    it 'does not set metadata profiles abilities' do
-      # When flexible metadata is disabled, the ability check should not interfere
-      # We don't check for specific abilities since they're not set
-      expect { subject }.not_to raise_error
+    it 'does not allow admin users to manage metadata profiles since search-only tenants have no data' do
+      is_expected.not_to be_able_to(:manage, :metadata_profiles)
+    end
+  end
+
+  context 'when no account is present' do
+    before do
+      allow(Site).to receive(:account).and_return(nil)
+    end
+
+    it 'allows admin users to manage metadata profiles (defaults to allowing on nil account)' do
+      is_expected.to be_able_to(:manage, :metadata_profiles)
     end
   end
 end

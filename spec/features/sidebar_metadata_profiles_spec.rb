ffdd910452422f852ef41b25a2e0f50ab11e00e8
Tracy A. McCormick <tracy.alan.mccormick@gmail.com>
2025-11-12 20:05:09 -0500

    current passing spec - slight refactor.

--- spec/features/sidebar_metadata_profiles_spec.rb
+++ spec/features/sidebar_metadata_profiles_spec.rb
@@ -7,49 +7,62 @@ RSpec.feature 'Dashboard sidebar metadata profiles link', type: :feature, clean:
 
   before do
     login_as user
-    allow(ENV).to receive(:fetch).with('HYRAX_FLEXIBLE', false).and_return(true)
+    allow(ENV).to receive(:fetch) do |key, default|
+      ENV[key] || default
+    end
   end
 
-  context 'when on a regular tenant' do
+  after(:each) do
+    # Reset ENV and reload routes after each test to avoid leaking state
+    ENV['HYRAX_FLEXIBLE'] = nil
+    Rails.application.reload_routes!
+  end
+
+  context 'when HYRAX_FLEXIBLE is enabled' do
     before do
+      ENV['HYRAX_FLEXIBLE'] = 'true'
+      Rails.application.reload_routes!
       allow(Site).to receive(:account).and_return(double(search_only?: false))
     end
 
     scenario 'displays the metadata profiles link' do
       visit hyrax.dashboard_path
-      
-      within('.sidebar') do
-        expect(page).to have_link('Metadata Profiles', href: metadata_profiles_path)
-      end
-    end
-  end
 
-  context 'when on a search-only tenant' do
-    before do
-      allow(Site).to receive(:account).and_return(double(search_only?: true))
-    end
-
-    scenario 'hides the metadata profiles link' do
-      visit hyrax.dashboard_path
-      
       within('.sidebar') do
-        expect(page).not_to have_link('Metadata Profiles')
-        expect(page).not_to have_content('Metadata Profiles')
+        expect(page).to have_link(I18n.t('hyrax.admin.sidebar.metadata_profiles'), href: /\/metadata_profiles/)
       end
     end
   end
 
   context 'when HYRAX_FLEXIBLE is disabled' do
     before do
-      allow(ENV).to receive(:fetch).with('HYRAX_FLEXIBLE', false).and_return(false)
+      ENV['HYRAX_FLEXIBLE'] = 'false'
+      Rails.application.reload_routes!
       allow(Site).to receive(:account).and_return(double(search_only?: false))
     end
 
     scenario 'hides the metadata profiles link' do
       visit hyrax.dashboard_path
-      
+
       within('.sidebar') do
-        expect(page).not_to have_link('Metadata Profiles')
+        expect(page).not_to have_link(I18n.t('hyrax.admin.sidebar.metadata_profiles'))
+      end
+    end
+  end
+
+  context 'when on a search-only tenant' do
+    before do
+      ENV['HYRAX_FLEXIBLE'] = 'true'
+      Rails.application.reload_routes!
+      allow(Site).to receive(:account).and_return(double(search_only?: true))
+    end
+
+    scenario 'hides the metadata profiles link' do
+      visit hyrax.dashboard_path
+
+      within('.sidebar') do
+        expect(page).not_to have_link(I18n.t('hyrax.admin.sidebar.metadata_profiles'))
+        expect(page).not_to have_content('Metadata Profiles')
       end
     end
   end
@@ -58,14 +71,15 @@ RSpec.feature 'Dashboard sidebar metadata profiles link', type: :feature, clean:
     let(:user) { create(:user) }
 
     before do
+      ENV['HYRAX_FLEXIBLE'] = 'true'
+      Rails.application.reload_routes!
       allow(Site).to receive(:account).and_return(double(search_only?: false))
     end
 
     scenario 'hides the metadata profiles link' do
       visit hyrax.dashboard_path
-      
-      # Non-admin users shouldn't see admin sidebar sections
-      expect(page).not_to have_link('Metadata Profiles')
+
+      expect(page).not_to have_link(I18n.t('hyrax.admin.sidebar.metadata_profiles'))
     end
   end
 end

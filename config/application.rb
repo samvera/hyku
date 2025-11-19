06b6e11e6a4dfde318fed23e10560b5b272d2407
Shana Moore <shana.lavina.moore@gmail.com>
2025-11-19 08:02:34 -0800

    :bug: Fix zeitwork deployment error
    Added rubocop to the ignore list so Zeitwerk won't autoload lib/rubocop
    
    Otherwise, deployments errored with
    
    "/usr/local/bundle/gems/zeitwerk-2.7.3/lib/zeitwerk/loader/callbacks.rb:31:in `on_file_autoloaded': expected file /app/samvera/hyrax-webapp/lib/rubocop/custom_cops.rb to define constant Rubocop::CustomCops, but didn't (Zeitwerk::NameError)
          raise Zeitwerk::NameError.new(msg, cref.cname)"

--- config/application.rb
+++ config/application.rb
@@ -150,7 +150,7 @@ module Hyku
     # Please, add to the `ignore` list any other `lib` subdirectories that do
     # not contain `.rb` files, or that should not be reloaded or eager loaded.
     # Common ones are `templates`, `generators`, or `middleware`, for example.
-    config.autoload_lib(ignore: %w[assets tasks middleware])
+    config.autoload_lib(ignore: %w[assets tasks middleware rubocop])
     config.add_autoload_paths_to_load_path = true
 
     ##

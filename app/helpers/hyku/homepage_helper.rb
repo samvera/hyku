0ea379ee18e652e17c332045ee3878b7f6593b7f
Sarah Proctor <sproctor950@gmail.com>
2026-01-30 09:55:46 -0800

    💄 Homepage featured and recent works refactor
    No functionality changes.
    
    Ref:
    - https://github.com/notch8/hyku-community-issues/issues/18

--- app/helpers/hyku/homepage_helper.rb
+++ app/helpers/hyku/homepage_helper.rb
@@ -2,10 +2,10 @@
 
 module Hyku
   module HomepageHelper
-    FEATURED_DESCRIPTION_LENGTH = 250
+    DESCRIPTION_LENGTH = 250
 
-    def truncate_featured_description(text)
-      truncate(text, length: FEATURED_DESCRIPTION_LENGTH, separator: ' ', omission: '...')
+    def truncate_description(text)
+      truncate(text, length: DESCRIPTION_LENGTH, separator: ' ', omission: '...')
     end
   end
 end

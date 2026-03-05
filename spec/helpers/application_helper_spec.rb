6f0802503a650b9190f630df810ee246035d8fec
LaRita Robinson <larita@notch8.com>
2026-03-04 19:42:44 -0500

    Use alt text on views

--- spec/helpers/application_helper_spec.rb
+++ spec/helpers/application_helper_spec.rb
@@ -1,6 +1,51 @@
 # frozen_string_literal: true
 
 RSpec.describe ApplicationHelper, type: :helper do
+  describe '#thumbnail_alt_text_for' do
+    let(:document) { SolrDocument.new('thumbnail_alt_text_tesim' => thumbnail_alt_text, 'title_tesim' => ['My Work']) }
+
+    context 'when the document has thumbnail_alt_text (real thumbnail)' do
+      let(:thumbnail_alt_text) { ['Custom alt text'] }
+
+      it 'returns alt_text_for_view from the document' do
+        expect(helper.thumbnail_alt_text_for(document)).to eq(document.alt_text_for_view)
+      end
+
+      it 'does not check the content block' do
+        expect(helper).not_to receive(:block_for)
+        helper.thumbnail_alt_text_for(document)
+      end
+    end
+
+    context 'when the document has no thumbnail_alt_text (default image showing)' do
+      let(:thumbnail_alt_text) { [] }
+
+      context 'when a content block is configured' do
+        before { allow(helper).to receive(:block_for).with(name: 'default_work_image_text').and_return('Site default alt') }
+
+        it 'returns the content block value' do
+          expect(helper.thumbnail_alt_text_for(document)).to eq('Site default alt')
+        end
+      end
+
+      context 'when no content block is configured' do
+        before { allow(helper).to receive(:block_for).with(name: 'default_work_image_text').and_return(false) }
+
+        it 'falls back to alt_text_for_view' do
+          expect(helper.thumbnail_alt_text_for(document)).to eq(document.alt_text_for_view)
+        end
+      end
+
+      context 'with a collection block_name' do
+        before { allow(helper).to receive(:block_for).with(name: 'default_collection_image_text').and_return('Collection default') }
+
+        it 'uses the provided block_name' do
+          expect(helper.thumbnail_alt_text_for(document, block_name: 'default_collection_image_text')).to eq('Collection default')
+        end
+      end
+    end
+  end
+
   describe "#markdown" do
     let(:header) { '# header' }
     let(:bold) { '*bold*' }

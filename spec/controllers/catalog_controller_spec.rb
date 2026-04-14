6bd9398238419e25abf192be500446ef00b1b2f1
April Rieger <april.rieger@yahoo.com>
2026-04-14 13:46:36 -0700

    add more test coverage

--- spec/controllers/catalog_controller_spec.rb
+++ spec/controllers/catalog_controller_spec.rb
@@ -1,6 +1,33 @@
 # frozen_string_literal: true
 
 RSpec.describe CatalogController do
+  # Hyku solr/conf/solrconfig.xml does not define per-field spellcheck dictionaries
+  # (e.g. creator, publisher); sending them caused Solr errors / Blacklight InvalidRequest.
+  describe "search field Solr params (spellcheck regression)" do
+    %w[
+      contributor
+      creator
+      title
+      description
+      publisher
+      date_created
+      subject
+      language
+      resource_type
+      format
+      identifier
+      based_near_label
+      keyword
+    ].each do |key|
+      it "does not set spellcheck.dictionary on #{key} search field" do
+        field = described_class.blacklight_config.search_fields[key]
+        expect(field).to be_present, "expected search_fields['#{key}'] to exist"
+        solr_keys = (field.solr_parameters || {}).stringify_keys.keys
+        expect(solr_keys).not_to include("spellcheck.dictionary")
+      end
+    end
+  end
+
   describe "GET /show" do
     let(:file_set) { create(:file_set) }
 

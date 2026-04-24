ad391005baaf145a53e89dc36cc01faf2401e614
Shana Moore <shana.lavina.moore@gmail.com>
2026-04-24 13:49:32 -0700

    sets chunk upload to persistent volume path
    points to willow_sword branch: 'i540-chunk-uploads'

--- config/initializers/willow_sword.rb
+++ config/initializers/willow_sword.rb
@@ -9,5 +9,7 @@ Rails.application.config.after_initialize do
     config.default_work_model = GenericWorkResource
     config.authorize_request = true
     config.xml_mapping_read = 'Hyku'
+    # Subdirectory of tmp/network_files (already on the shared uploads PVC in k8s: friends, production, etc.)
+    config.chunked_upload_path = Rails.root.join('tmp', 'network_files', 'willow_sword').to_s
   end
 end

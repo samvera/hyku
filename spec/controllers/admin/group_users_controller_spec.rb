02c0a58720ee9476e0fec1e3525f5bdd910c7e4d
Max Kadel <max@notch8.com>
2026-09-17 12:45:22 +0200

    Fix GHSA-qvw7-2cfx-c54q / GHSA-7cq7-84q2-f23j: admin group-role authorization

--- spec/controllers/admin/group_users_controller_spec.rb
+++ spec/controllers/admin/group_users_controller_spec.rb
@@ -7,7 +7,7 @@ RSpec.describe Admin::GroupUsersController, faketenant: true do
     describe 'GET #index' do
       subject { get :index, params: { group_id: group.id } }
 
-      it { is_expected.to redirect_to new_user_session_path }
+      it { is_expected.to redirect_to root_path }
     end
   end
 
@@ -40,6 +40,57 @@ RSpec.describe Admin::GroupUsersController, faketenant: true do
             delete :destroy, params: { group_id: group.id, user_id: user.id }
           end.to change(group.members, :count).by(-1)
         end
+      end
+    end
+  end
+
+  context 'as a low-privilege signed-in user (privilege escalation)' do
+    let(:low_priv_user) { FactoryBot.create(:user) }
+    let(:admin_group) { FactoryBot.create(:admin_group) }
+
+    before { sign_in low_priv_user }
+
+    describe 'POST #create' do
+      it 'does not let the user add themself to the admin group' do
+        expect do
+          post :create, params: { group_id: admin_group.id, user_id: low_priv_user.id }
+        end.not_to change(admin_group.members, :count)
+      end
+
+      it 'does not grant admin ability as a side effect of the request' do
+        post :create, params: { group_id: admin_group.id, user_id: low_priv_user.id }
+        expect(Ability.new(low_priv_user.reload).admin?).to be false
+      end
+    end
+
+    describe 'DELETE #destroy' do
+      let(:other_group) { FactoryBot.create(:group, member_users: [low_priv_user, victim]) }
+      let(:victim) { FactoryBot.create(:user) }
+
+      it 'does not let the user remove another member from a group they do not manage' do
+        expect do
+          delete :destroy, params: { group_id: other_group.id, user_id: victim.id }
+        end.not_to change(other_group.members, :count)
+      end
+    end
+  end
+
+  context 'as a user_manager (non-admin, admin-delegable role)' do
+    let(:user_manager) { FactoryBot.create(:user_manager) }
+    let(:admin_group) { FactoryBot.create(:admin_group) }
+
+    before { sign_in user_manager }
+
+    describe 'POST #create' do
+      it 'does not let a user_manager add themself to the admin group' do
+        expect do
+          post :create, params: { group_id: admin_group.id, user_id: user_manager.id }
+        end.not_to change(admin_group.members, :count)
+      end
+
+      it 'does not grant admin ability as a side effect of the request' do
+        post :create, params: { group_id: admin_group.id, user_id: user_manager.id }
+        expect(Ability.new(user_manager.reload).admin?).to be false
       end
     end
   end
